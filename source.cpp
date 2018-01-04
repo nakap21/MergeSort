6af88c09f08d5d1fe7f14d94e3d469bef1a0d24f
nakap21 <31996406+nakap21@users.noreply.github.com>
2018-01-04 17:05:49 +0300

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,78 +1,73 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
-using namespace std;
-
+template <typename T>
 struct ownList {
-    int val;
+    T val;
     struct ownList *next;
 };
 
-
-ownList* merge(ownList* first, ownList *second) {
-    ownList* help = nullptr;
-    if (first->val < second->val) {
-        help = first;
-        first = first->next;
-    } else {
-        help = second;
-        second = second->next;
-    }
-    ownList* ret = help;
+template <typename T>
+ownList<T>* mergeSort(ownList<T>* first, ownList<T>* second) {
+    ownList<T>* help = new ownList<T>;
+    ownList<T>* ret = help;
     while(first != nullptr && second != nullptr) {
         if (first->val < second->val) {
             help->next = first;
             first = first->next;
-            help = help->next;
         } else {
             help->next = second;
             second = second->next;
-            help = help->next;
         }
+        help = help->next;
     }
     if (first == nullptr) {
         help->next = second;
     } else {
         help->next = first;
     }
+    help = ret;
+    ret = ret->next;
+    delete help;
     return ret;
 }
 
-ownList* divi(ownList *now, int siz) {
-    if (siz <= 1) {
+template <typename T>
+ownList<T>* merge(ownList<T>* now) {
+    if (now == nullptr || now->next == nullptr) {
         return now;
     }
-    ownList *second = now;
-    int firSiz = siz / 2;
-    for (int i = 0; i < siz / 2; ++i) {
-        if (i == siz / 2 - 1) {
-            ownList *tmp = second;
-            second = second->next;
-            tmp->next = nullptr;
-        } else {
-            second = second->next;
-        }
+    ownList<T>* second = now;
+    ownList<T>* tmp = now;
+    while (tmp->next != nullptr && tmp->next->next != nullptr) {
+        second = second->next;
+        tmp = tmp->next;
+        tmp = tmp->next;
     }
-    return merge(divi(now, firSiz), divi(second, siz - firSiz));
+    tmp = second;
+    second = second->next;
+    tmp->next = nullptr;
+    return mergeSort(merge(now), merge(second));
 }
 
-int main()
-{
-    ownList* head = new ownList;
+int main() {
+    ownList<int>* head = new ownList<int>;
     int n;
-    cin >> n;
+    std::cin >> n;
     head = nullptr;
     for (int i = 0; i < n; ++i) {
-        int what;
-        cin >> what;
-        ownList* tmp = new ownList;
-        tmp->val = what;
+        int element;
+        ownList<int>* tmp = new ownList<int>;
+        std::cin >> element;
+        tmp->val = element;
         tmp->next = head;
         head = tmp;
     }
-    head = divi(head, n);
+    head = merge(head);
     while (head != nullptr) {
-        cout << head->val << " ";
+        std::cout << head->val << " ";
+        ownList<int>* forDelete = head;
         head = head->next;
+        delete forDelete;
     }
     return 0;
 }
